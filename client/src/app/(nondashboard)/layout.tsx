a8ff1bbd426988cfe7e405405b49d44640c7973e
RJ Tabelon <tabelonrj@gmail.com>
2025-09-12 21:43:44 -0400

    Implement manager-specific redirection on search and home pages

--- client/src/app/(nondashboard)/layout.tsx
+++ client/src/app/(nondashboard)/layout.tsx
@@ -1,14 +1,36 @@
-"use client";
+'use client';
 
 import Navbar from '@/components/Navbar';
 import { NAVBAR_HEIGHT } from '@/lib/constants';
 import { useGetAuthorUserQuery } from '@/state/api';
-import React from 'react';
+import { usePathname, useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const { data: authUser} = useGetAuthorUserQuery();
-  console.log('authUser:', authUser)
-  
+  const { data: authUser, isLoading: authLoading } = useGetAuthorUserQuery();
+  const router = useRouter();
+  const pathname = usePathname();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (authUser) {
+      const userRole = authUser.userRole?.toLowerCase();
+      if (
+        (userRole === 'manager' && pathname.startsWith('/search')) ||
+        (userRole === 'manager' && pathname === "/")
+      ) {
+        router.push(
+          "/managers/properties",
+          { scroll: false }
+        );
+      } else {
+        setIsLoading(false);
+      }
+    }
+  }, [authUser, router, pathname]);
+
+  if (authLoading || isLoading) return <>Loading...</>;
+
   return (
     <div className='h-full w-full'>
       <Navbar />
